c7bb457d931ddc240d3429db9cf84c872a516c97
Krzysztof-lab <kpieczka@student.agh.edu.pl>
2025-04-06 18:53:14 +0200

    changed to counter

--- client.py
+++ client.py
@@ -1,85 +1,58 @@
 import pygame
 from network import Network
 
+pygame.init()
+
 width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNumber = 0
+font = pygame.font.SysFont("comicsans", 50)
 
-
-class Player():
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x,y,width,height)
-        self.vel = 3
+class Player:
+    def __init__(self, counter=0):
+        self.counter = counter
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
+        win.fill((255, 255, 255))
+        text = font.render(f"Counter: {self.counter}", True, (0, 0, 0))
+        win.blit(text, (150, 200))
+        pygame.display.update()
 
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-
-def redrawWindow(win,player, player2):
-    win.fill((255,255,255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
-
+    def update_counter(self, value):
+        self.counter = value
 
 def main():
     run = True
-    n = Network()
-    startPos = read_pos(n.getPos())
-    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
-    p2 = Player(0,0,100,100,(255,0,0))
     clock = pygame.time.Clock()
+    player = Player()
+    n = Network()
 
     while run:
         clock.tick(60)
-        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
+        try:
+            # Wysyłamy lokalny licznik
+            data = n.send(str(player.counter))
+            # Serwer zwraca dane w formacie "licznik,coś_innego"
+            # Parsujemy pierwszą część jako licznik
+            received_values = data.split(",")
+            other_counter = int(received_values[0])
+            player.update_counter(other_counter)
+        except Exception as e:
+            run = False
+            print("Connection lost or error:", e)
+            break
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
+            # Jeśli kliknięto lewy przycisk myszy, zwiększamy licznik
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                player.counter += 1
 
-        p.move()
-        redrawWindow(win, p, p2)
+        player.draw(win)
+
+    pygame.quit()
 
 main()
--- server.py
+++ server.py
@@ -2,60 +2,68 @@ import socket
 from _thread import *
 import sys
 
-server = "10.129.11.127"
+server = "192.168.0.157"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
-    s.bind((server, port))
+	s.bind((server, port))
 except socket.error as e:
-    str(e)
+	print(str(e))
+	sys.exit()
 
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+# Globalny licznik
+global_counter = 0
+# Lista aktywnych połączeń
+clients = []
 
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
+def threaded_client(conn):
+	global global_counter
+	clients.append(conn)
 
-pos = [(0,0),(100,100)]
+	# Na początku wysyłamy aktualny stan licznika do nowego klienta
+	try:
+		conn.send(str(global_counter).encode())
+	except:
+		pass
 
-def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
-    reply = ""
-    while True:
-        try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+	while True:
+		try:
+			data = conn.recv(2048).decode()
+			if not data:
+				print("Disconnected")
+				break
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = pos[0]
-                else:
-                    reply = pos[1]
+			# Próbujemy skonwertować odebrane dane na liczbę.
+			# Zakładamy, że klient wysyła nową wartość licznika.
+			try:
+				new_value = int(data)
+				global_counter = new_value
+			except ValueError:
+				print("Otrzymano nieprawidłowe dane:", data)
+				continue
 
-                print("Received: ", data)
-                print("Sending : ", reply)
+			print("Nowy globalny licznik:", global_counter)
+			# Rozsyłamy nowy stan licznika do wszystkich klientów
+			for client in clients:
+				try:
+					client.send(str(global_counter).encode())
+				except:
+					pass
+		except Exception as e:
+			print("Exception:", e)
+			break
 
-            conn.sendall(str.encode(make_pos(reply)))
-        except:
-            break
+	print("Lost connection")
+	clients.remove(conn)
+	conn.close()
 
-    print("Lost connection")
-    conn.close()
 
-currentPlayer = 0
 while True:
-    conn, addr = s.accept()
-    print("Connected to:", addr)
-
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+	conn, addr = s.accept()
+	print("Connected to:", addr)
+	start_new_thread(threaded_client, (conn,))
