8118a3da0d610001ce98b994abed870a8b4236ab
MacFras <mfras0857@gmail.com>
2025-08-09 00:26:47 +0200

    optimise test

--- src/model/abilities/commanders/optimise.py
+++ src/model/abilities/commanders/optimise.py
@@ -1,10 +1,13 @@
 from overrides import overrides
 
 from src.model.abilities.commanders.commander_base import CommanderAbilityBase
+from src.model.enums.row_type import RowType
 
 
 class Optimise(CommanderAbilityBase):
     @overrides
     def on_board_play(self, game, player, row_type, targets):
-        #TODO doxlarise
+        row, _ = game.board.get_row(RowType.CLOSE, player.id)
+        for card in row.cards:
+            print(card)
         return
